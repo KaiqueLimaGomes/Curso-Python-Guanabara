46890ef2d457be4ec1c5eb125485f8dc719a4631
Kaique Lima <kaiquelimag@gmail.com>
2026-01-26 22:39:58 -0300

    StartCurso
    Primeiro dia de estudo, conhecendo e instalando as ferramentas.

---
+++ Aula04/teste01.py
@@ -0,0 +1,6 @@
+print('------ DESAFIO 01 ------')
+nome = input('Qual é o seu nome? ')
+idade = input('Qual é a sua idade? ')
+peso = input('Qual o seu peso? ')
+print(nome, idade, peso)
+
---
+++ Aula04/teste03.py
@@ -0,0 +1,5 @@
+print('------ DESAFIO 03 ------')
+numA = int(input('Digite o primeiro número: '))
+numB = int(input ('Digite o segundo número: '))
+soma = numA + numB
+print('A soma é ', soma)
