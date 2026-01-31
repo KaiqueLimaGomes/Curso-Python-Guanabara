bf7c2cc16439d9c01ea287024066630c189e6278
Kaique Lima <kaiquelimag@gmail.com>
2026-01-31 20:21:18 -0300

    Aula09
    Desenvolvimento Aula 09

---
+++ Aula09/Aula09a.py
@@ -0,0 +1,21 @@
+frase = 'Curso em Vídeo Python'
+print(frase) #Imprime a string armazenada
+print(frase[3]) #imprime o caracter da terceira possição 'S'
+print(frase[3:13]) #imprime do terceiro caracter até 12, o 13 não contabiliza
+print(frase[:10]) #imprime do inicio até o caracter 9, o 10 não cantabiliza
+print(frase[10:]) #imprime da posição 10 até o final
+print(frase[1:15:2]) #impre da posição 1 até a 14 pulando de 2 em 2, o 15 não contabiliza
+print(frase[::2]) #imprime do inicio ao fim pulando de 2 em 2 posição
+
+print(""" um 
+texto 
+grande 
+de 
+varias 
+linhas
+ aqui""")  # 3x" para imprimir um texto grande com um unico print
+
+print(frase.count('o')) #Conta quantos caracteres semelhante o texto possui, Possui diferença em letra ALTA e baixa
+print(frase.upper().count('O')) #Deixa o texto em caixa ALTA e procura todos os 'O'
+print(len(frase)) #Conta quantidade de caracteres da string, inclusive espaços vazios antes e depois do texto
+print(frase.strip()) #Remove espaços vazios indesejados antes e depois do texto, não remove o espaço entre palavras
